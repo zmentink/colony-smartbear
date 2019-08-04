99ea56cec75ee6c63690db4fbb1c57f62cf49a53
alexazarh <aazarh@gmail.com>
2019-08-04 15:11:53 +0300

    renaming "smartbear-servicev" app to "salesforce-virt"

--- applications/salesforce-virt/salesforce-virt.sh
+++ applications/salesforce-virt/salesforce-virt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "==> deploy war file artifact"
-mv /root/smartbear-servicev/artifacts/*.war /usr/local/apache-tomcat9/webapps
+mv /root/salesforce-virt/artifacts/*.war /usr/local/apache-tomcat9/webapps
 
 echo "==> start tomcat"
 /usr/local/apache-tomcat9/bin/startup.sh
